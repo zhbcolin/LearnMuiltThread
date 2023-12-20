59d613946d3f8dba4c8eaccbc72ce968c88dec7f
zhbcolin <zhanghzobo@163.com>
2023-12-20 22:01:51 +0800

    day03-3

--- test/test4.cpp
+++ test/test4.cpp
@@ -108,3 +108,22 @@ void precision_lock() {
     lock.lock();
     ++shared_data;
 }
+
+class DNService {
+public:
+    DNService();
+    std::string QueryDNS(std::string dnsname) {
+        std::shared_lock<std::shared_mutex> shared_locks(_shared_mtx);
+        auto iter = _dns_info.find(dnsname);
+        if (iter != _dns_info.end()) return iter->second;
+        return "";
+    }
+    void AddDNSInfo(std::string dnsname, std::string dnsentry) {
+        std::lock_guard<std::shared_mutex>  guard_locks(_shared_mtx);
+        _dns_info.insert(std::make_pair(dnsname, dnsentry));
+    }
+
+private:
+    std::map<std::string, std::string> _dns_info;
+    mutable std::shared_mutex  _shared_mtx;
+};
